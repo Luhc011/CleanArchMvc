98027cecdaeb6573e99ab5f1c8abfcbba702226c
Lucas H <lucashcardoso26@gmail.com>
2023-08-16 11:13:30 -0300

    update productDTO

--- CleanArchMvc.Application/DTOs/ProductDTO.cs
+++ CleanArchMvc.Application/DTOs/ProductDTO.cs
@@ -5,43 +5,43 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CleanArchMvc.Application.DTOs
 {
-    public class ProductDTO
-    {
-        public int Id { get; set; }
+	public class ProductDTO
+	{
+		public int Id { get; set; }
 		public Category? Category { get; set; }
 
 
 		[Required(ErrorMessage = "Name is required")]
-        [MinLength(3)]
-        [MaxLength(100)]
-        [DisplayName("Name")]
-        public string? Name { get; private set; }
+		[MinLength(3)]
+		[MaxLength(100)]
+		[DisplayName("Name")]
+		public string? Name { get; set; }
 
-        [Required(ErrorMessage = "Description is required")]
-        [MinLength(5)]
-        [MaxLength(200)]
-        [DisplayName("Description")]
-        public string? Description { get; private set; }
+		[Required(ErrorMessage = "Description is required")]
+		[MinLength(5)]
+		[MaxLength(200)]
+		[DisplayName("Description")]
+		public string? Description { get;  set; }
 
-        [Required(ErrorMessage = "Price is required")]
-        [Column(TypeName = "decimal(14,2)")]
-        [DisplayFormat(DataFormatString = "{0:C2}")]
-        [DataType(DataType.Currency)]
-        [DisplayName("Price")]
-        public decimal Price { get; private set; }
+		[Required(ErrorMessage = "Price is required")]
+		[Column(TypeName = "decimal(14,2)")]
+		[DisplayFormat(DataFormatString = "{0:C2}")]
+		[DataType(DataType.Currency)]
+		[DisplayName("Price")]
+		public decimal Price { get;  set; }
 
-        [Required(ErrorMessage = "Stock is required")]
-        [Range(1, 9999)]
-        [DisplayName("Stock")]
-        public int Stock { get; private set; }
+		[Required(ErrorMessage = "Stock is required")]
+		[Range(1, 9999)]
+		[DisplayName("Stock")]
+		public int Stock { get;  set; }
 
-        [MaxLength(250)]
-        [DisplayName("Product Image")]
-        public string? Image { get; private set; }
+		[MaxLength(250)]
+		[DisplayName("Product Image")]
+		public string? Image { get;  set; }
 
 
-        [DisplayName("Categories")]
-        public int CategoryId { get; set; }
+		[DisplayName("Categories")]
+		public int CategoryId { get; set; }
 
-    }
+	}
 }
